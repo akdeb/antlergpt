c6b587fa699fde0978a4d0ac199086a9b933801c
akdeb <deb2@illinois.edu>
2023-10-19 17:02:57 -0500

    works well

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import "@radix-ui/themes/styles.css";
-import { Theme, ThemePanel } from "@radix-ui/themes";
+import { Theme } from "@radix-ui/themes";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}><Theme appearance="light" accentColor="red" panelBackground="solid" radius="none"><ThemePanel />{children}</Theme></body>
+      <body className={inter.className}><Theme appearance="light" accentColor="red" panelBackground="solid" radius="none">{children}</Theme></body>
     </html>
   )
 }
