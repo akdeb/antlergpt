7edae330cbbaae7baa70ccf6e76e01a268d8e8f2
akdeb <deb2@illinois.edu>
2023-10-20 12:43:37 -0500

    website works well

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,9 +1,11 @@
+"use client";
+
 import { Button, Flex } from "@radix-ui/themes";
 
 export const Banner = () => {
     return <div style={{ width: "100%", position: "absolute",top: 0,padding: 10 }}>
         
-        <Flex justify={"end"}><Button size="4">
+        <Flex justify={"end"}><Button size="4" onClick={() => {alert("Talk to 5000 more customers first")}}>
         Click for $250,000 in funding <span style={{ fontSize: 30 }}>🤑</span>
       </Button>
             </Flex>
